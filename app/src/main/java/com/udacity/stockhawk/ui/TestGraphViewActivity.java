161ebe1d8cf0307ed6a70b35b5aad242a44761cd
liberostrategies <liberostrategies@gmail.com>
2016-12-02 13:29:21 -0500

    Add GraphView package test.

---
+++ app/src/main/java/com/udacity/stockhawk/ui/TestGraphViewActivity.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016. Libero Strategies, LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and Confidential
+ */
+
+package com.udacity.stockhawk.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.udacity.stockhawk.R;
+
+/**
+ * Created by pink on 12/2/2016.
+ */
+
+public class TestGraphViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.test_graphview);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+    }
+}
